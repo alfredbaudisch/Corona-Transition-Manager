793f6bd8f08102b4a0a27df38c903df5f7a944cb
Alfred R. Baudisch <alfred.r.baudisch@gmail.com>
2011-11-09 10:07:44 -0200

    Added a small timer manager (cancels all active timers).

---
+++ timerManager.lua
@@ -0,0 +1,39 @@
+--[[
+	TIMER MANAGER
+		
+	Just a helper to allow an easy way to cancel all ongoing timers when switching screens, etc.
+	Example:
+	local timers = require("timerManager")
+	timers:add(1000, someFunctionToCall)
+	timers:add(2000, someFunctionToCall2)
+	
+	.. requested to change scene, even with timers going on:
+	timers:cancelAll()
+	
+	NOTE: If you make changes/updates, feel free to send them to me, so we keep improving the class :)
+
+	@author Alfred R. Baudisch <alfred.r.baudisch@gmail.com>
+ 	@url http://www.karnakgames.com
+	@version 0.1b, 09/Nov/2011 
+ 	@license http://creativecommons.org/licenses/by/3.0/		
+]]--
+local timers = {
+	db = {}
+}
+
+function timers:add(time, callback)
+	table.insert(self.db, timer.performWithDelay(time, callback))
+end
+
+function timers:cancelAll()
+	local amountTimers = #self.db
+
+	for i = 1, amountTimers do
+		if self.db[i] ~= nil then
+			timer.cancel(self.db[i])
+			self.db[i] = nil
+		end
+	end
+end
+
+return timers
